eb3c8f78ea4d109d398b64880ff23bf5aa65a7fe
Eric Podolsky <ericpodolsky@Erics-MacBook-Pro.local>
2019-09-16 18:25:43 -0400

    Remove all files from master

--- app/src/main/java/com/ericpodolsky/tennisscore/models/PlayerModel.kt
+++
@@ -1,9 +0,0 @@
-package com.ericpodolsky.tennisscore.models
-
-data class PlayerModel(
-        val name: String,
-        var isServer: Boolean,
-        var rawGameScore: Int = 0,
-        var gamesWon: Int = 0,
-        var setsWon: Int = 0,
-        var matchWon: Boolean = false)
--- app/src/main/java/com/ericpodolsky/tennisscore/models/TennisScore.kt
+++
@@ -1,3 +0,0 @@
-package com.ericpodolsky.tennisscore.models
-
-data class TennisScores(val player1Score: String, val player2Score: String)
